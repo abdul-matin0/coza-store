00ab3af336c9ffb7330e0d92b071676b8982e0dc
abdul-matin0 <madeayo04@gmail.com>
2020-12-16 21:06:00 +0100

    scaffolding for identity

--- Coza.Utility/SD.cs
+++ Coza.Utility/SD.cs
@@ -5,5 +5,10 @@ namespace Coza.Utility
     public static class SD
     {
         // static class for utils
+
+        // roles
+        public const string Role_User_Indi = "Individual Customer";
+        public const string Role_Employee = "Employee";
+        public const string Role_Admin = "Admin";
     }
 }
---
+++ Coza/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Coza.DataAccess.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Coza.Areas.Identity.IdentityHostingStartup))]
+namespace Coza.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
