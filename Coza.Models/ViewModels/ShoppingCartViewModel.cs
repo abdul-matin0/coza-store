73845e3730e14e1f0376a81f9d5d3223117a9aba
abdul-matin0 <madeayo04@gmail.com>
2020-12-24 12:35:17 +0100

    shopping cart vm and index

--- Coza.Models/ViewModels/ShoppingCartViewModel.cs
+++ Coza.Models/ViewModels/ShoppingCartViewModel.cs
@@ -8,5 +8,7 @@ namespace Coza.Models.ViewModels
     {
         public IEnumerable<Product> ProductList { get; set; }
         public ShoppingCart CartObj { get; set; }
+        public IEnumerable<ShoppingCart> ListCart { get; set; }
+        public OrderHeader OrderHeader { get; set; }
     }
 }
---
+++ Coza/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Coza.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Coza.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            // view list of all products in cart
+            _unitOfWork.ShoppingCart.GetAll();
+            return View();
+        }
+    }
+}
