3f92f59de77f9163001593d4eebd0f8215ee1b58
abdul-matin0 <madeayo04@gmail.com>
2020-12-26 08:10:30 +0100

    cart controller function

--- Coza/Areas/Customer/Controllers/CartController.cs
+++ Coza/Areas/Customer/Controllers/CartController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Coza.DataAccess.Repository.IRepository;
+using Coza.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Coza.Areas.Customer.Controllers
@@ -18,9 +20,27 @@ namespace Coza.Areas.Customer.Controllers
 
         public IActionResult Index()
         {
-            // view list of all products in cart
-            _unitOfWork.ShoppingCart.GetAll();
-            return View();
+            //get logged in user
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            // display list of all products in cart based on user
+            ShoppingCartViewModel shoppingCartVM = new ShoppingCartViewModel()
+            {
+                ListCart = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUser.Id == claims.Value, includeProperties: "Product"),
+                OrderHeader = new Models.OrderHeader()
+            };
+
+            shoppingCartVM.OrderHeader.OrderTotal = 0;
+
+            foreach(var items in shoppingCartVM.ListCart)
+            {
+                items.Price = (items.Product.Price * items.Count);
+                shoppingCartVM.OrderHeader.OrderTotal += (items.Price);
+
+            }
+
+            return View(shoppingCartVM);
         }
     }
 }
