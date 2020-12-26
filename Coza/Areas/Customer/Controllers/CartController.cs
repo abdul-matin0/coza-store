e7aa00fef4e65b954cfa990b2eca1c2a5a06455f
abdul-matin0 <madeayo04@gmail.com>
2020-12-26 10:00:25 +0100

    action buttons on index page for cart controller

--- Coza/Areas/Customer/Controllers/CartController.cs
+++ Coza/Areas/Customer/Controllers/CartController.cs
@@ -5,12 +5,15 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Coza.DataAccess.Repository.IRepository;
 using Coza.Models.ViewModels;
+using Coza.Utility;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Coza.Areas.Customer.Controllers
 {
     [Area("Customer")]
+    [Authorize]
     public class CartController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
@@ -19,7 +22,6 @@ namespace Coza.Areas.Customer.Controllers
             _unitOfWork = unitOfWork;
         }
 
-        [Authorize]
         public IActionResult Index()
         {
             //get logged in user
@@ -43,6 +45,47 @@ namespace Coza.Areas.Customer.Controllers
             }
 
             return View(shoppingCartVM);
+        }
+
+        public IActionResult Plus(int id) {
+            var cartItem = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == id, includeProperties: "Product");
+            cartItem.Count += 1;
+
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Minus(int id)
+        {
+            var cartItem = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == id, includeProperties: "Product");
+
+            if(cartItem.Count == 1)
+            {
+                _unitOfWork.ShoppingCart.Remove(cartItem);
+                int currentNumOfItem = (int)HttpContext.Session.GetInt32(SD.ssShoppingCart);
+                HttpContext.Session.SetInt32(SD.ssShoppingCart, currentNumOfItem - 1);
+            }
+            else
+            {
+                cartItem.Count -= 1;
+
+            }
+
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove(int id)
+        {
+            var cartItem = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == id, includeProperties: "Product");
+
+            _unitOfWork.ShoppingCart.Remove(cartItem);
+            _unitOfWork.Save();
+
+            int currentNumOfItem = (int)HttpContext.Session.GetInt32(SD.ssShoppingCart);
+            HttpContext.Session.SetInt32(SD.ssShoppingCart, currentNumOfItem - 1);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
