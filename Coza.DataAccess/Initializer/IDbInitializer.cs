91c71ed9a239a953011b9c3a739b398845918ef2
abdul-matin0 <madeayo04@gmail.com>
2021-01-08 22:30:31 +0100

    db initializer

---
+++ Coza.DataAccess/Initializer/IDbInitializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coza.DataAccess.Initializer
+{
+    public interface IDbInitializer
+    {
+        void Initialize();
+    }
+}
